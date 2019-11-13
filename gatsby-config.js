47833f92d3859d58109bb915630e00ebc6b1e528
Laust Deleuran <ljd@ljd.dk>
2019-11-13 09:38:42 -0800

    fix: add htaccess for 404s

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
 		image: 'src/images/smelborp-og.png',
 	},
 	plugins: [
+		'gatsby-plugin-htaccess',
 		'gatsby-plugin-react-helmet',
 		{
 			resolve: 'gatsby-source-filesystem',
